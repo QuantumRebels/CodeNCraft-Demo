1dbf79c8d2f86bc8e4555ebe877e9d34a63621f6
Chandan-kr-dev <chandanramgar@gmail.com>
2024-11-08 23:06:44 +0530

    updates

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,60 +1,108 @@
-import express from 'express';
-import { Users } from '../models/user.models';
-import bcrypt from 'bcryptjs'
+import express from "express";
+import { Users } from "../models/user.models";
+import bcrypt from "bcryptjs";
 
+const login = async (req, res) => {
+  const { Email, password, userrole } = req.body;
+  console.log(Email, password);
 
-const login=async(req,res)=>{
-    const {Email,password,userrole}=req.body;
-    console.log(Email,password);
+  try {
+    const user = await Users.find({ Email: Email });
 
-    try {
-        const user=await Users.find({Email:Email})
-        
-        if(user.userrole==userrole){
-            const match=await bcrypt.compare(user.password,password)
-            if(match){
-                res.json({
-                    user:user,
-                    message:"Success"
-                })
-            }
-            else{
-                res.status(400).json({message:"Passsword Incorrect"})
-            }
-        }
-        else{
-            res.status(400).json({message:"User not found Or You are not a Admin"})
-        }
-
-    } catch (error) {
-        res.status(404).json("Server Error")
+    if (user.userrole == userrole) {
+      const match = await bcrypt.compare(user.password, password);
+      if (match) {
+        res.json({
+          user: user,
+          message: "Success",
+        });
+      } else {
+        res.status(400).json({ message: "Passsword Incorrect" });
+      }
+    } else {
+      res
+        .status(400)
+        .json({ message: "User not found Or You are not a Admin" });
     }
-}
+  } catch (error) {
+    res.status(404).json("Server Error");
+  }
+};
 
-const Signup=async(req,res)=>{
-    const {Name,Email,password,UserRole}=req.body;
-    try {
-        
-        
-            const existingUser=await Users.findOne({Email:Email})
-            if(existingUser){
-                return res.status(400).json({message:"User already exists ..Please Login"})
-            }else{
-
-                const hashedPassword=await bcrypt.hash(password,10)
-                const newUser=new Users({
-                    Name,
-                    Email,
-                    password:hashedPassword,
-                    userRole:UserRole
-                })
-                await newUser.save()
-                res.json({message:"User registered successfully"})
-            }
-        }
-    catch (error) {
-        res.status(500).json("Server Error")
+const Signup = async (req, res) => {
+  const { Username, Email,Phone, password } = req.body;
+  try {
+    const existingUser = await Users.findOne({ Email: Email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User already exists ..Please Login" });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new Users({
+        Name: Username,
+        Email: Email,
+        PhoneNumber:Phone,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.json({ message: "User registered successfully" });
     }
-}
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+};
 
-export default {login,Signup}
+const SignupAdmin = async (req, res) => {
+  const { Username, Email,Phone, password } = req.body;
+  try {
+    const existingUser = await Users.findOne({ Email: Email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Staff already exists .." });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new Users({
+        Name: Username,
+        Email: Email,
+        PhoneNumber:Phone,
+        password: hashedPassword,
+        userRole:"Department Staff"
+      });
+      await newUser.save();
+      res.json({ message: "Staff registered successfully" });
+    }
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+};
+
+const SignupInvertoryStaff= async (req, res) => {
+  const { Username, Email,Phone, password } = req.body;
+  try {
+    const existingUser = await Users.findOne({ Email: Email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User already exists .." ,
+            existingUser
+        });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new Users({
+        Name: Username,
+        Email: Email,
+        PhoneNumber:Phone,
+        password: hashedPassword,
+        userRole:"Invertory Staff"
+      });
+      await newUser.save();
+      res.json({ message: "Staff registered successfully" });
+    }
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+};
+
+export default { login, Signup ,SignupInvertoryStaff,SignupAdmin};
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,104 +1,184 @@
 
-// LoginForm.js
 import React, { useState } from 'react';
-import { AiOutlineMail, AiOutlineLock, AiOutlineGoogle } from 'react-icons/ai';
-import { FaApple } from 'react-icons/fa';
+
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import { GoogleAuthProvider, OAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 const LoginForm = () => {
-    const [role, setRole] = useState('user');
+ const [Email, setEmail] = useState("")
+ const [Password, setPassword] = useState("")
+ const [showPassword, setShowPassword] = useState(false)
+ const [rememberMe, setrememberMe] = useState(false)
+ const [userRole, setuserRole] = useState("")
+ 
 
-    return (
-        <div className="max-w-lg mx-auto p-12 mt-12 shadow-2xl rounded-lg bg-white">
-            <h2 className="text-4xl font-bold text-[#114fee] text-center mb-6">Sign In</h2>
-            
-            {/* Role Selection */}
-            <div className="mb-4">
-                <label className="block text-gray-700 font-medium mb-2" htmlFor="role">
-                    Login as
-                </label>
-                <select
-                    id="role"
-                    className="w-full p-2.5 border rounded-lg focus:outline-none "
-                    value={role}
-                    onChange={(e) => setRole(e.target.value)}
-                >
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
-                    <option value="inventory">Inventory Manager</option>
-                </select>
+ const [Error, setError] = useState("")
+ const [Loader, setLoader] = useState(false)
+
+ const navigate=useNavigate()
+
+ 
+
+ 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      setLoader(true)
+      axios.post(`${import.meta.env.VITE_DEV_URL}users/login`,{Email,Password,userRole}) 
+      .then(res=>{
+        console.log(res.data)
+        if(res.data.message=="Success"){
+          setLoader(false)
+          alert("Login Successful")
+          window.localStorage.setItem("Username",res.data.user.Name)
+          navigate('/')
+
+        }else{
+          setError(res.data)
+        }
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    } catch (error) {
+        console.error(error)
+    }
+    
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center background-login">
+      <form onSubmit={handleSubmit} className="w-full max-w-md space-y-6 rounded-lg form-login  p-8 shadow-lg">
+        {/* Email Input */}
+        <div className="space-y-2">
+          <h1 className='text-3xl lg:text-4xl my-2 flex justify-center items-center py-2 font-semibold tracking-tighter bg-gradient-to-b from-neutral-50 via-neutral-300 to-neutral-700 bg-clip-text text-transparent'>Login</h1>
+          <label className="text-sm font-medium text-gray-400">Email</label>
+          <div className="relative">
+            <div className="pointer-events-none absolute inset-y-0 left-3 flex items-center">
+              <svg height="20" viewBox="0 0 32 32" width="20" className="text-gray-500">
+                <g><path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z" /></g>
+              </svg>
             </div>
-
-            {/* Email Field */}
-            <div className="mb-4">
-                <label className="block text-gray-700 font-medium mb-2" htmlFor="email">
-                    Email
-                </label>
-                <div className="flex items-center border rounded-lg p-2.5">
-                    <AiOutlineMail className="text-gray-500 mr-2" />
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Enter your Email"
-                        className="w-full outline-none"
-                    />
-                </div>
-            </div>
-
-            {/* Password Field */}
-            <div className="mb-4">
-                <label className="block text-gray-700 font-medium mb-2" htmlFor="password">
-                    Password
-                </label>
-                <div className="flex items-center border rounded-lg p-2.5">
-                    <AiOutlineLock className="text-gray-500 mr-2" />
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Enter your Password"
-                        className="w-full outline-none"
-                    />
-                </div>
-            </div>
-
-            {/* Remember Me and Forgot Password */}
-            <div className="flex items-center justify-between mb-6">
-                <label className="flex items-center text-gray-600">
-                    <input type="checkbox" className="mr-2" />
-                    Remember me
-                </label>
-                <a href="/forgot-password" className="text-[#114fee] hover:underline">
-                    Forgot password?
-                </a>
-            </div>
-
-            {/* Sign In Button */}
-            <button className="w-full bg-black text-white py-2 rounded-lg font-semibold hover:bg-gray-800 mb-4">
-                Sign In
-            </button>
-
-            {/* Sign Up Link */}
-            <p className="text-center text-gray-600 mb-4">
-                Don't have an account?{' '}
-                <a href="/signup" className="text-[#114fee] hover:underline">
-                    Sign Up
-                </a>
-            </p>
-
-            {/* Divider */}
-            <div className="flex items-center justify-center my-4">
-                <span className="text-gray-500 mx-2">Or With</span>
-            </div>
-
-            {/* Social Login Buttons */}
-            <div className="flex justify-center space-x-4">
-                <button className="flex items-center px-4 py-2 border rounded-lg hover:bg-gray-100">
-                    <AiOutlineGoogle className=" mr-2" />
-                    Continue With Google
-                </button>
-                
-            </div>
+            <input
+              type="email"
+              name="email"
+              value={Email}
+              onChange={e=>setEmail(e.target.value)}
+              className="w-full text-black rounded-md border border-gray-300 py-2 pl-10 pr-4 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              placeholder="Enter your Email"
+            />
+          </div>
         </div>
-    );
+
+        {/* Password Input */}
+        <div className="space-y-2">
+          <label className="text-sm font-medium text-gray-400">Password</label>
+          <div className="relative">
+            <div className="pointer-events-none absolute inset-y-0 left-3 flex items-center">
+              <svg height="20" viewBox="-64 0 512 512" width="20" className="text-gray-500">
+                <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0" /><path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0" />
+              </svg>
+            </div>
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={Password}
+              onChange={e=>setPassword(e.target.value)}
+              className="w-full text-black rounded-md border border-gray-300 py-2 pl-10 pr-10 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              placeholder="Enter your Password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-3 flex items-center"
+            >
+              <svg viewBox="0 0 576 512" height="1em" className="text-gray-500">
+                <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* Remember Me & Forgot Password */}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              name="rememberMe"
+              checked={rememberMe}
+              onChange={e=>setrememberMe(e.target.checked)}
+              className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+            />
+            <label className="ml-2 text-sm text-gray-400">Remember me</label>
+          </div>
+          <button type="button" className="text-sm font-medium text-blue-600 hover:text-blue-500">
+            Forgot password?
+          </button>
+        </div>
+
+        {/* Sign In Button */}
+        <button
+        disabled={Loader==true}
+          type="submit"
+          className="w-full rounded-md bg-blue-600 py-2 text-white transition-colors hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          {Loader ? (<span>Signing in</span>):(<span>Sign In</span>)}
+        
+        </button>
+        <div className='text-center'>
+          <span className='text-red-600 text-xs'>{Error}</span>
+        </div>
+
+        {/* Sign Up Link */}
+        <p className="text-center text-sm text-gray-400">
+          Don't have an account?{' '}
+          <a href='/signup' className="font-medium text-blue-600 hover:text-blue-500">
+            Sign Up
+          </a>
+        </p>
+
+        {/* Divider */}
+        <div className="relative">
+
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 text-gray-500">Or With</span>
+          </div>
+        </div>
+
+        {/* Social Buttons */}
+        <div className="grid grid-cols-2 gap-4">
+          <button
+            type="button"
+            onClick={GoogleLogin}
+            className="flex items-center justify-center gap-2 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            <svg width="20" height="20" viewBox="0 0 512 512">
+              <path fill="#FBBB00" d="M113.47,309.408L95.648,375.94l-65.139,1.378C11.042,341.211,0,299.9,0,256c0-42.451,10.324-82.483,28.624-117.732h0.014l57.992,10.632l25.404,57.644c-5.317,15.501-8.215,32.141-8.215,49.456C103.821,274.792,107.225,292.797,113.47,309.408z" />
+              <path fill="#518EF8" d="M507.527,208.176C510.467,223.662,512,239.655,512,256c0,18.328-1.927,36.206-5.598,53.451c-12.462,58.683-45.025,109.925-90.134,146.187l-0.014-0.014l-73.044-3.727l-10.338-64.535c29.932-17.554,53.324-45.025,65.646-77.911h-136.89V208.176h138.887L507.527,208.176L507.527,208.176z" />
+              <path fill="#28B446" d="M416.253,455.624l0.014,0.014C372.396,490.901,316.666,512,256,512c-97.491,0-182.252-54.491-225.491-134.681l82.961-67.91c21.619,57.698,77.278,98.771,142.53,98.771c28.047,0,54.323-7.582,76.87-20.818L416.253,455.624z" />
+              <path fill="#F14336" d="M419.404,58.936l-82.933,67.896c-23.335-14.586-50.919-23.012-80.471-23.012c-66.729,0-123.429,42.957-143.965,102.724l-83.397-68.276h-0.014C71.23,56.123,157.06,0,256,0C318.115,0,375.068,22.126,419.404,58.936z" />
+            </svg>
+            Google
+          </button>
+          <button
+            type="button"
+            onClick={AppleLogin}
+            className="flex items-center justify-center gap-2 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            <svg width="20" height="20" viewBox="0 0 22.773 22.773">
+              <path d="M15.769,0c0.053,0,0.106,0,0.162,0c0.13,1.606-0.483,2.806-1.228,3.675c-0.731,0.863-1.732,1.7-3.351,1.573c-0.108-1.583,0.506-2.694,1.25-3.561C13.292,0.879,14.557,0.16,15.769,0z" />
+              <path d="M20.67,16.716c0,0.016,0,0.03,0,0.045c-0.455,1.378-1.104,2.559-1.896,3.655c-0.723,0.995-1.609,2.334-3.191,2.334c-1.367,0-2.275-0.879-3.676-0.903c-1.482-0.024-2.297,0.735-3.652,0.926c-0.155,0-0.31,0-0.462,0c-0.995-0.144-1.798-0.932-2.383-1.642c-1.725-2.098-3.058-4.808-3.306-8.276c0-0.34,0-0.679,0-1.019c0.105-2.482,1.311-4.5,2.914-5.478c0.846-0.52,2.009-0.963,3.304-0.765c0.555,0.086,1.122,0.276,1.619,0.464c0.471,0.181,1.06,0.502,1.618,0.485c0.378-0.011,0.754-0.208,1.135-0.347c1.116-0.403,2.21-0.865,3.652-0.648c1.733,0.262,2.963,1.032,3.723,2.22c-1.466,0.933-2.625,2.339-2.427,4.74C17.818,14.688,19.086,15.964,20.67,16.716z" />
+            </svg>
+            Apple
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
